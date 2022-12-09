627650603fd98bff352c9391b51db78178cb9e6c
Eskil Refsgaard <eskil.refsgaard@gmail.com>
2022-12-09 17:01:46 +0100

    changes

---
+++ script.js
@@ -0,0 +1,69 @@
+let elPara = document.querySelector("#p_txt1") 
+
+elPara.addEventListener('click', (event) => {
+    elPara.innerHTML += 'a'; 
+    //Denne skal endres slik at det du faktisk skriver,
+    //skal skrives inn i boksen
+  
+});
+
+let elTest = document.querySelector("#test");
+  
+elTest.addEventListener('blur', (event) => {
+    console.log("blur")
+    elLabel = document.querySelector("#txtLabel");
+    elLabel.innerHTML = elTest.value;
+})
+
+
+function theTest() {
+    let elTable2 = document.querySelector("#test");
+    
+    elTable2.click = hmrConsole();  //?????, jeg har så mange spærsmål
+}
+
+function hmrConsole() {
+    console.log("Hello HmR")
+
+}
+
+
+
+function printFirstAndLastName() {
+    const elFirst = document.querySelector("#firstname").value;
+    const elLast = document.querySelector("#lastname").value
+
+    console.log("Fist and last name: " + elFirst + elLast);
+}
+
+function showList() {
+    const elNames = document.querySelectorAll(".elName");
+
+    for (const elName of elNames) {
+        if (elName.style.visibility === 'visible') {
+            elName.style.visibility = 'hidden';
+            
+        }else{   
+            elName.style.visibility = 'visible';
+
+        }
+    }
+}
+
+function addToTable(){
+    const Fname = document.querySelector("#firstname");
+    const Lname = document.querySelector("#lastname");
+    let elTable = document.querySelector("#tableElements");
+
+    elTable.innerHTML += '<tr> <th class="l1">' + Fname.value + '</th> <th class="l1">' + Lname.value + '</th> </tr>';
+
+}
+
+function removeTable() {
+
+    console.log("Av en eller annen merkelig grunn på man trygger på rm btn 2 ganger ved de som er pre langt inn")
+    let elTable = document.querySelector("#tableElements");
+    elTable.lastChild.remove();
+}
+
+
